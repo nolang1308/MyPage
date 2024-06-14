db4e0fd23c0ce35cdc1df84935aeded6a0111189
DOTORY <suminhwang1308@gmail.com>
2024-06-14 17:45:27 +0900

    도토리
    배경바꾸기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import DotoryCard from "./DotoryCard/DotoryCard";
+import Glass from "./Glass/Glass";
 
 function App() {
     const toSite=()=>{
@@ -11,6 +12,9 @@ function App() {
     return (
         <div className="App">
             <DotoryCard/>
+            <div className={"glass"}>
+                <Glass/>
+            </div>
 
 
         </div>
---
+++ src/Glass/Glass.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Glass.css';
+
+const Glass = () => {
+    return (
+        <div className="glass-effect">
+            <h1>유리창 효과</h1>
+            <p>이것은 반투명 유리창 효과를 적용한 박스입니다.</p>
+        </div>
+    );
+}
+
+export default Glass;
